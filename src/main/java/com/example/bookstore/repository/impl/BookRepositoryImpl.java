7f43d299097ca83bc9cd569c7bd946f3bb94c7cf
Dmytro Sokolovskyi <dmytro.sokolovskyi@icloud.com>
2024-03-25 23:10:11 -0400

    added book controller, dto, mapstruct & mappers. added metods to BookService. added EntityNotFoundException

--- src/main/java/com/example/bookstore/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/example/bookstore/repository/impl/BookRepositoryImpl.java
@@ -3,20 +3,17 @@ package com.example.bookstore.repository.impl;
 import com.example.bookstore.model.Book;
 import com.example.bookstore.repository.BookRepository;
 import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+@RequiredArgsConstructor
 @Repository
 public class BookRepositoryImpl implements BookRepository {
     private final SessionFactory sessionFactory;
-
-    @Autowired
-    public BookRepositoryImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
 
     @Override
     public Book save(Book book) {
@@ -47,6 +44,15 @@ public class BookRepositoryImpl implements BookRepository {
                     "FROM Book", Book.class).getResultList();
         } catch (Exception e) {
             throw new RuntimeException("Can't retrieve books from DB", e);
+        }
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            return Optional.ofNullable(session.find(Book.class, id));
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find book by id from DB: " + id, e);
         }
     }
 }
