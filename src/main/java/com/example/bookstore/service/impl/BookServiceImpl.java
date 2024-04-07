39b32430a58256c80114166cdd34b7d615696475
Dmytro Sokolovskyi <dmytro.sokolovskyi@icloud.com>
2024-04-06 23:48:05 -0400

    add minor improvement after review

--- src/main/java/com/example/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/bookstore/service/impl/BookServiceImpl.java
@@ -43,13 +43,9 @@ public class BookServiceImpl implements BookService {
         Book book = bookRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("Can't find book by id: " + id)
         );
-        book.setTitle(requestDto.getTitle());
-        book.setAuthor(requestDto.getAuthor());
-        book.setIsbn(requestDto.getIsbn());
-        book.setPrice(requestDto.getPrice());
-        book.setDescription(requestDto.getDescription());
-        book.setCoverImage(requestDto.getCoverImage());
-        return bookMapper.toDto(bookRepository.save(book));
+        Book updatedBook = bookMapper.toModel(requestDto);
+        updatedBook.setId(id);
+        return bookMapper.toDto(bookRepository.save(updatedBook));
     }
 
     @Override
