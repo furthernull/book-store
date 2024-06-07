f2e706ede4991a1b087d6dd590965bd805d07d91
Dmytro Sokolovskyi <dmytro.sokolovskyi@icloud.com>
2024-06-07 01:31:52 -0400

    added security-related functionality, basic authentication, roles, endpoints role-related access

--- src/main/java/com/example/bookstore/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/com/example/bookstore/exception/CustomGlobalExceptionHandler.java
@@ -42,6 +42,24 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         return new ResponseEntity<>(body, HttpStatus.CONFLICT);
     }
 
+    @ExceptionHandler(RoleNotFoundException.class)
+    protected ResponseEntity<Object> handleRoleNotFoundException(RoleNotFoundException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.NOT_IMPLEMENTED);
+        body.put("error", "registration cannot be extended " + ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.NOT_FOUND);
+        body.put("error", ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex,
--- src/main/java/com/example/bookstore/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/bookstore/service/impl/UserServiceImpl.java
@@ -3,18 +3,26 @@ package com.example.bookstore.service.impl;
 import com.example.bookstore.dto.user.UserRegistrationRequestDto;
 import com.example.bookstore.dto.user.UserResponseDto;
 import com.example.bookstore.exception.RegistrationException;
+import com.example.bookstore.exception.RoleNotFoundException;
 import com.example.bookstore.mapper.UserMapper;
+import com.example.bookstore.model.Role;
 import com.example.bookstore.model.User;
+import com.example.bookstore.repository.role.RoleRepository;
 import com.example.bookstore.repository.user.UserRepository;
 import com.example.bookstore.service.UserService;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
+    private static final Role.RoleName DEFAULT_ROLE = Role.RoleName.USER;
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
@@ -24,6 +32,10 @@ public class UserServiceImpl implements UserService {
                     + "%s already exists", requestDto.getEmail()));
         }
         User user = userMapper.toModel(requestDto);
+        user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        user.setRoles(Set.of(roleRepository.findByRole(DEFAULT_ROLE).orElseThrow(
+                () -> new RoleNotFoundException(String.format("Can't find %s role",
+                        DEFAULT_ROLE)))));
         userRepository.save(user);
         return userMapper.toDto(user);
     }
